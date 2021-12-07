c3e8422830a44b31e802613104611a41b23b0c28
Colt Pini <colt@otus.com>
2021-12-07 11:02:13 -0700

    each app is running seperatly now.

--- apps/builder/src/app/remote-entry/entry.module.ts
+++ apps/builder/src/app/remote-entry/entry.module.ts
@@ -1,6 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { RemoteEntryComponent } from './entry.component';
@@ -8,7 +8,7 @@ import { RemoteEntryComponent } from './entry.component';
 @NgModule({
   declarations: [RemoteEntryComponent],
   imports: [
-    BrowserModule,
+    CommonModule,
     FormsModule,
     RouterModule.forChild([
       {
